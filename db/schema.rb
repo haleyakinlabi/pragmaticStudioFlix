1ad2e68d2d61ed52ec645c4fa4f4684294858e61
Haley Akinlabi (haleyakinlabi) <haley.akinlabi@clickfunnels.com>
2024-04-02 09:45:57 -0600

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_01_183721) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_02_153458) do
   create_table "movies", force: :cascade do |t|
     t.string "name"
     t.string "rating"
     t.decimal "total_gross"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
+    t.datetime "released_on"
   end
 
 end
