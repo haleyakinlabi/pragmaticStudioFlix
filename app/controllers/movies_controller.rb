6cb05539cde0d9de68b56ef988744522f24a39ae
Nate Hopkins (hopsoft) <natehop@gmail.com>
2024-05-14 10:56:13 -0600

    Editing Records Part Two

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,15 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+     movie_params = params.require(:movie).permit(:name, :rating, :total_gross, :description, :released_on)
+     @movie.update(movie_params)
+    redirect_to @movie
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root "movies#index"
   get "movies" => "movies#index"
   get "movies/:id" => "movies#show", as: "movie"
+  get "movie/:id/edit" => "movies#edit", as: "edit_movie"
+  patch "movies/:id" => "movies#update"
 end
